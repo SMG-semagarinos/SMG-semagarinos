118aa4df758b4fad9db9a66bdff4b4e9f09e3764
Sebastián E. Magariños <SMagarinos@outlook.com>
2021-06-16 13:31:45 -0300

    NuevosScripts

---
+++ Office365/SharepointSites.ps1
@@ -0,0 +1,11 @@
+﻿Install-Module -Name Microsoft.Online.SharePoint.PowerShell
+
+$adminUPN="sebastianmagarinos@MACRO.COM.AR"
+$orgName="macro1"
+Connect-SPOService -Url https://$orgName-admin.sharepoint.com 
+
+Get-SPOSite -Template REDIRECTSITE#0
+Get-SPOSite
+
+Get-SPODeletedSite 
+Remove-SPODeletedSite https://macro1.sharepoint.com/sites/ImplementacionSkype
---
+++ Telegram/Ejemplos.ps1
@@ -0,0 +1,3 @@
+$token = “#########:xxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxx”
+$chat = “-#########”
+$test = "I am a test"
